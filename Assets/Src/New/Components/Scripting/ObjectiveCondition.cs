062fd381e5c4bb936772e943667645b4b4d9513e
Lewis Wrigley <lewis@arenaflowers.com>
2020-02-08 14:57:41 +0000

    Add primary objective to first level

--- Assets/Src/New/Components/Scripting/ObjectiveCondition.cs
+++ Assets/Src/New/Components/Scripting/ObjectiveCondition.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public abstract class ObjectiveCondition : MonoBehaviour {
 
     public abstract bool satisfied { get; }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.3f);
+    }
 }
