80ed1ea50219d83d645d7094ad42ab2d3c26221c
BATMAN\lewis <lewis.wrigley@gmail.com>
2025-03-24 15:53:12 +0000

    show which enemies are primaries and which are secondary

--- Assets/Scripts/Monobehaviours/UI/AlienIcon.cs
+++ Assets/Scripts/Monobehaviours/UI/AlienIcon.cs
@@ -3,9 +3,12 @@ using UnityEngine.UI;
 
 public class AlienIcon : MonoBehaviour {
     
+    public GameObject primaryIndicator;
+    
     AlienData alien;
     
-    public void SetAlien(AlienData alien) {
+    public void SetAlien(AlienData alien, bool primary) {
+        primaryIndicator.SetActive(primary);
         this.alien = alien;
         Instantiate(Resources.Load<AlienImage>("Prefabs/AlienSprites/Images/" + alien.name + "Image"), transform);
     }
--- Assets/Scripts/Monobehaviours/UI/MissionPreview.cs
+++ Assets/Scripts/Monobehaviours/UI/MissionPreview.cs
@@ -5,14 +5,14 @@ public class MissionPreview : MonoBehaviour {
     public Transform alienIconPrototype;
     
     void Start() {
-        foreach (var profile in Mission.current.enemyProfiles.primaries) InstantiateIcon(profile);
-        foreach (var profile in Mission.current.enemyProfiles.secondaries) InstantiateIcon(profile);
+        foreach (var profile in Mission.current.enemyProfiles.primaries) InstantiateIcon(profile, true);
+        foreach (var profile in Mission.current.enemyProfiles.secondaries) InstantiateIcon(profile, false);
         alienIconPrototype.gameObject.SetActive(false);
     }
     
-    void InstantiateIcon(EnemyProfile profile) {
+    void InstantiateIcon(EnemyProfile profile, bool primary) {
         var iconTrans = Instantiate(alienIconPrototype, alienIconPrototype.parent);
         var icon = iconTrans.GetComponent<AlienIcon>();
-        icon.SetAlien(AlienData.Get(profile.typeName));
+        icon.SetAlien(AlienData.Get(profile.typeName), primary);
     }
 }
